3851e02a33115b82a51a0aba01f7888ea29fa840
Toto Corvidoni <TotoCorvidoni@gmail.com>
2018-10-02 12:57:15 -0300

    Add #show and @board examples

--- spec/chess_game_spec.rb
+++ spec/chess_game_spec.rb
@@ -1,8 +1,33 @@
 require 'chess_game'
 
 describe ChessGame do
-  describe '#setup prepares the game' do
-    it { is_expected.to respond_to(:setup) }
-      
+  it { is_expected.to respond_to(:setup) }
+
+  # describe '#setup prepares the game' do
+  # end
+
+  describe '@board to represent the board' do
+
+    it 'consist in an array of 8 nested arrays' do
+      expect(subject.board).to be_kind_of(Array)
+      expect(subject.board.map { |x| x.size }.all?(8)).to be true
     end
+  end  
+
+  describe '#show displays the state of @board' do
+    context 'at the start of the game' do
+      it 'displays the setted up board' do
+        expect { subject.show }.to output(puts(<<~BOARD)).to_stdout
+          |♜|♞|♝|♛|♚|♝|♞|♜|
+          |♟|♟|♟|♟|♟|♟|♟|♟|
+          |⛚|⛚|⛚|⛚|⛚|⛚|⛚|⛚|
+          |⛚|⛚|⛚|⛚|⛚|⛚|⛚|⛚|
+          |⛚|⛚|⛚|⛚|⛚|⛚|⛚|⛚|
+          |⛚|⛚|⛚|⛚|⛚|⛚|⛚|⛚|
+          |♙|♙|♙|♙|♙|♙|♙|♙|
+          |♖|♘|♗|♕|♔|♗|♘|♖|
+        BOARD
+      end
+    end      
+  end
 end
