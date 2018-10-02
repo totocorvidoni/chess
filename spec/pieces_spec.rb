4bcc9a3c157c1954d767a22c9046c9d5351334d5
Toto Corvidoni <TotoCorvidoni@gmail.com>
2018-10-02 16:41:34 -0300

    Add rook examples

--- spec/pieces_spec.rb
+++ spec/pieces_spec.rb
@@ -27,10 +27,37 @@ describe King do
     end
   end
 
-  context 'when it moves outside the boord' do
+  context 'when it moves outside the board' do
     it 'is not valid' do
       expect(king.valid_move?([4, -1])).to be false
     end
   end
 end
 
+describe Rook do
+  subject(:rook) { Rook.new('♜', [7, 0]) }
+
+  context 'when it moves forward' do
+    it 'is valid' do
+      expect(rook.valid_move?([7, 7])).to be true
+    end    
+  end  
+
+  context 'when it moves left' do
+    it 'is valid' do
+      expect(rook.valid_move?([0, 0])).to be true
+    end
+  end
+
+  context 'when it moves diagonally' do
+    it 'is not valid' do
+      expect(rook.valid_move?([5, 2])).to be false
+    end
+  end
+
+  context 'when it moves out of the board' do
+    it 'is not valid' do
+      expect(rook.valid_move?([8, 7])).to be false      
+    end    
+  end
+end
