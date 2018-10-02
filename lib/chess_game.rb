c77f50dfb7f9aca2f9ae0db2dedb7e7153d02f20
Toto Corvidoni <TotoCorvidoni@gmail.com>
2018-10-02 11:14:07 -0300

    Add #setup (pending Pieces class) and #show

--- lib/chess_game.rb
+++ lib/chess_game.rb
@@ -2,8 +2,26 @@ require_relative 'pieces.rb'
 require_relative 'player.rb'
 
 class ChessGame
+  attr_reader :board
+
+  WHITE = { king: '♚',
+            queen: '♛',
+            bishop: '♝',
+            knight: '♞',
+            rook: '♜',
+            pawn: '♟' }
+
+  BLACK = { king: '♔',
+            queen: '♕',
+            bishop: '♗',
+            knight: '♘',
+            rook: '♖',
+            pawn: '♙' }
+
+  EMPTY = '⛚'
+
   def initialize
-    
+    setup
   end
 
   def move_piece 
@@ -16,12 +34,20 @@ class ChessGame
   end
 
   def setup
-    @grid = Array.new(8) { Array.new }
+    @board = []
+    @board[0] = [BLACK[:rook], BLACK[:knight], BLACK[:bishop], BLACK[:queen], BLACK[:king], BLACK[:bishop], BLACK[:knight], BLACK[:rook]]
+    @board[1] = Array.new(8) { BLACK[:pawn] }
+    @board.fill(2..5) { Array.new(8) { EMPTY } }
+    @board[6] = Array.new(8) { WHITE[:pawn] }
+    @board[7] = [WHITE[:rook], WHITE[:knight], WHITE[:bishop], WHITE[:queen], WHITE[:king], WHITE[:bishop], WHITE[:knight], WHITE[:rook]]
   end
 
   def show
+    @board.each do |rank|
+      puts "|#{rank.join('|')}|"
+    end
   end
-  
+
   def new_player(name, color)
     Player.new(name, color)
   end
